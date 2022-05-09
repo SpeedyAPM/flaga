59c0c89d4cf8c8482c9a87602e5e52de07b49a29
SpeedyAPM <mipsdeb@gmail.com>
2022-05-09 21:25:14 +0000

    baza pod zadanie domowe

---
+++ eraseh.py
@@ -0,0 +1,10 @@
+import os
+
+os.system("sudo rm -r static/hero_image")
+os.system("sudo rm -r saved_heroes")
+os.system("sudo rm -r hero_think")
+
+# sudo tail -f -s 4 /var/log/syslog
+# export FLASK_APP=app.py
+# source /var/www/flaga/flagaenv/bin/activate
+# flask run
